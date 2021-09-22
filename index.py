1951c06fa8102be7bbc43e38f312702886c281de
D3spina <61208907+D3spina@users.noreply.github.com>
2021-09-22 07:48:00 +0200

    Update index.py

--- index.py
+++ index.py
@@ -12,12 +12,12 @@ async def on_ready():
 
 @bot.command()
 async def helpme(ctx):
-	"""afficher la liste des commandes disponibles"""
+	"""command to send message about bot's command"""
 	await ctx.send("Voici les commandes disponibles :\n - definition mot_clé : obtenir la définition d'un mot clé (anglais uniquement). la commande '$definition liste' affiche la liste des mots clés.\n - carte : permet d'afficher l'image des cartes recherché (anglais uniquement). Exemple '$carte Ms.'")
 
 @bot.command()
 async def definition(ctx, arg):
-	""" obtenir la définition d'un mot clé """
+	"""commend to obtain a key world's description """
 
 	"""lancement de bs4 pour récupérer les définitions"""
 	url_definition = "https://hallofheroeslcg.com/marvel-champions-lcg-keyword-list/"
@@ -31,26 +31,26 @@ async def definition(ctx, arg):
 			liste_definition.append(mot)
 		await ctx.send(liste_definition)
 	else:
-		""" vu la construction du html, on récupère le <p> suivant la balise contenant notre recherche"""
+		""" we get the next <p> after the <h3> who contains our search """
 		definitions = soup_definition.find(text = arg).findNext("p").contents
 		await ctx.send(definitions)
 
 @bot.command()
 async def carte(ctx, *arg):
-	""" obtenir l'image de la carte """
+	""" command to see a pull of search card (english) """
 
-	""" On élimine la recherche à 0 """
+	""" break the search without name of card """
 	if len(arg) == 0:
 		await ctx.send("Tu n'as pas préciser de carte!")
 
 	else:
 
-		""" on transforme le tuple en variable str """
+		""" tupl become a str """
 		recherche = ' '.join(arg) 
-		""" on transforme le str pour l'intégrer au lien"""
+		""" str formating """
 		recherche.lower().replace(" " , "+")
 
-		""""utilisation de bs4 pour récupérer toutes les images """
+		"""" request the marvelcdb base search with our search """
 		url_carte = 'https://marvelcdb.com/find?q=' + recherche + '&sort=set&view=card&decks=all'
 		page_carte = requests.get(url_carte)
 		soup_carte = BeautifulSoup(page_carte.content, 'html5lib')
@@ -58,17 +58,19 @@ async def carte(ctx, *arg):
 		carte = soup_carte.find_all("img")
 
 		
-		"""boucle for pour afficher chaque image"""
+		""" send every image fund in the precedent request """
 		for image in carte:
 			await ctx.send("https://marvelcdb.com/" + image['src'])
 
 @bot.command()
 async def random(ctx):
-	""" afficher le lien du randomizer de Halion """
+	""" command to send the Halion's randomizer """
 	await ctx.send("Vous pouvez randomizer une partie ici : https://canthom.github.io/randomizer-mc/")
 
 @bot.command()
 async def cartefr(ctx, *arg):
+	
+	""" WIP : search a french card """
 
 	""" Definition de arg en str pour recherche. Création de la variable dict pour le resultat"""
 	recherche = ' '.join(arg)
@@ -80,16 +82,14 @@ async def cartefr(ctx, *arg):
 
 	else:
 
-		""" ouverture du json"""
+		"""ouverture du json"""
 		with open("data.json", encoding ="utf8") as json_file:
 			data = json.load(json_file)
 
-		""""on rajoute chaque id octgn dans le dictionnaire resultat_carte de la recherche"""
+		""""on rajoute chaque id octgn dans le dictionnaire resultat_carte"""
 		for i in data:
 			if i['real_name'] == recherche:
-				if i['octgn_id'] not in data:
-					break
-				else:
+				if i['octgn_id'] in data[i]:
 					resultat_carte.append(i['octgn_id'])
 
 		""" on envoie chaque image """
